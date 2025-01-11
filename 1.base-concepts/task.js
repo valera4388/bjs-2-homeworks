ba289f8ed67a8a9f594de81abad0ca7ca775ccd7
mikhalev valera <mihalev.valera@mail.ru>
2025-01-11 12:14:16 +0300

    Первое задание полностью готово (второе решение)

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,40 +19,60 @@ function solveEquation(a, b, c) {
 		arr.push((-b + Math.sqrt(diskriminant)) / (2 * a));
 		arr.push((-b - Math.sqrt(diskriminant)) / (2 * a));
 	}
-
   
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
-  percent = +percent;
-	if (isNaN(percent) || (percent < 0) || (percent > 100)) {
-		return 0;
-	}
+// Преобразуем процент в месячный и в диапазон от 0 до 1
+let monthlyRate = (percent / 100) / 12;
 
-	contribution = +contribution;
-	if (isNaN(contribution) || contribution < 0) {
-		return 0;
-	}
+// Рассчитываем тело кредита
+let loanBody = amount - contribution;
 
-	amount = +amount;
-	if (isNaN(amount) || amount <= 0) {
-		return 0;
-	}
+// Если тело кредита меньше или равно нулю, возвращаем 0
+if (loanBody <= 0) {
+    return 0;
+}
 
-	countMonths = +countMonths;
-	if (isNaN(countMonths) || countMonths <= 0) {
-		return 0;
-	}
+// Используем формулу для расчета ежемесячного платежа
+let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
 
-	let p = percent / 100 / 12;
+// Общая сумма, выплаченная клиентом
+let totalAmount = monthlyPayment * countMonths;
 
-	let s = amount - contribution;
-	let sumOfPayments = 0;
+// Округляем результат до двух знаков после запятой
+return Math.round(totalAmount * 100) / 100;
 
-	sumOfPayments = (countMonths * s * (p + (p / (Math.pow((1 + p), countMonths) - 1)))).toFixed(2);
 
-	return +sumOfPayments;
+  // percent = +percent;
+	// if (isNaN(percent) || (percent < 0) || (percent > 100)) {
+	// 	return 0;
+	// }
+
+	// contribution = +contribution;
+	// if (isNaN(contribution) || contribution < 0) {
+	// 	return 0;
+	// }
+
+	// amount = +amount;
+	// if (isNaN(amount) || amount <= 0) {
+	// 	return 0;
+	// }
+
+	// countMonths = +countMonths;
+	// if (isNaN(countMonths) || countMonths <= 0) {
+	// 	return 0;
+	// }
+
+	// let p = percent / 100 / 12;
+
+	// let s = amount - contribution;
+	// let sumOfPayments = 0;
+
+	// sumOfPayments = (countMonths * s * (p + (p / (Math.pow((1 + p), countMonths) - 1)))).toFixed(2);
+
+	// return +sumOfPayments;
   
 }
