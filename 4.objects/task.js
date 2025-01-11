59eeaa7ed82426042e4efd5b87d637c7b10bae52
mikhalev valera <mihalev.valera@mail.ru>
2025-01-11 20:11:43 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
+    this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
   
 }
 
 Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
   
 }
 
 Student.prototype.addMarks = function (...marks) {
+    if (this.hasOwnProperty("marks")) {
+		this.marks = [...this.marks, ...marks];
+	}
   
 }
 
 Student.prototype.getAverage = function () {
+    if (this.hasOwnProperty("marks")) {
+		return this.marks.reduce((acc, item, i, arr) => acc + item / arr.length, 0);
+	} else {
+		return 0;
+	}
   
 }
 
 Student.prototype.exclude = function (reason) {
+    delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
   
 }
