8fadc1661b26fe70c23222cbd09975cb529f4d5c
mikhalev valera <mihalev.valera@mail.ru>
2025-01-11 15:00:40 +0300

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length === arr2.length) {
+		return arr1.every((element, index) => element === arr2[index]);
+	} else {
+		return false;
+	}
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter(user => user.gender === gender);
+
+    if (filteredUsers.length === 0) {
+      return 0;
+    }
+    const ages = filteredUsers.map(user => user.age);
+    const totalAge = ages.reduce((sum, age) => sum + age, 0);
+    const averageAge = totalAge / filteredUsers.length;
   
+    return +averageAge.toFixed(1);
+
 }
