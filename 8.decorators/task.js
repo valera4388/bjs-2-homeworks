c89ba0f0ba72fec885646f7bcc79471c41eb17b4
mikhalev valera <mihalev.valera@mail.ru>
2025-01-27 21:12:13 +0300

    Восьмое задание полностью готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,40 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+    let cache = [];
+    function wrapper(...args) {
+      const hash = md5(args);
+      let objectInCache = cache.find( item => item.hash === hash);
+      if (objectInCache) {
+        return `Из кеша: ${objectInCache.value}`;
+      }
+      let result = func(...args);
+      cache.push({"hash": hash, "value": result});
+      if (cache.length > 5) {
+        cache.shift();
+      }
+      return `Вычисляем: ${result}`;
+    }
+    return wrapper;
+  }
   
-}
-
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
-}
+  //Задача № 2
+  function debounceDecoratorNew(func, delay) {
+    let timeoutId;
+    let isTrottled = false;
+    function wrapper(...args) {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout( () => {
+        func(args);
+        wrapper.count++;
+      }, delay);
+      if (!isTrottled) {
+        func(...args);
+        wrapper.count++;
+        isTrottled = true;
+      }
+      wrapper.allCount++;
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
+  }
